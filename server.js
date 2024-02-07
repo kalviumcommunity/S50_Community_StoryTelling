0be099d9a9fb4d77de7fab12e945283f04254205
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-02-07 14:36:33 +0530

    Updated Server

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
 
 app.get("/ping", (req, res) => {
+  console.log("kk", port);
   res.send("<h1>pong</h1>");
 });
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`🚀 server running on PORT: ${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`🚀 server running on PORT: ${port}`);
+});
 
 module.exports = app;
