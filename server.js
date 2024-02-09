4c14d5d8017bdba7e284cf7e98e73e8f97954949
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-02-09 15:17:41 +0530

    Added Error Handling and Bruno Test

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get("/ping", (req, res) => {
   res.send("<h1>pong</h1>");
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 const server = app.listen(port, () => {
   console.log(`🚀 server running on PORT: ${port}`);
 });
